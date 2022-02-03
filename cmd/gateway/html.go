681fe7b43db2f451c45d3e603a001c68d5f2c736
Patrick Rudolph <patrick.rudolph@9elements.com>
2022-02-03 20:00:15 +0100

    cmd/gateway: Add application to forward data to HTML
    Signed-off-by: Patrick Rudolph <siro@das-labor.org>

---
+++ cmd/gateway/html.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/siro20/p1p2decoder/pkg/p1p2"
+)
+
+func runHtml(sys p1p2.System) {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.tmpl")
+	router.Static("/assets", "./assets")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title":        "DAIKIN Heat pump",
+			"temperatures": sys.Temperatures,
+			"valves":       sys.Valves,
+			"pumps":        sys.Pumps,
+			"status":       sys.Status,
+		})
+	})
+	rest := router.Group("/api/v1")
+	rest.GET("/", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, sys)
+	})
+	rest.GET("/temperatures", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, sys.Temperatures)
+	})
+	rest.GET("/valves", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, sys.Valves)
+	})
+	rest.GET("/pumps", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, sys.Pumps)
+	})
+	rest.GET("/status", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, sys.Status)
+	})
+
+	rest.GET("/temperatures/:name", func(c *gin.Context) {
+		for i := range sys.Temperatures {
+			if sys.Temperatures[i].Name() == c.Params.ByName("name") {
+				c.IndentedJSON(http.StatusOK, sys.Temperatures[i])
+
+			}
+		}
+	})
+	rest.GET("/valves/:name", func(c *gin.Context) {
+		for i := range sys.Valves {
+			if sys.Valves[i].Name() == c.Params.ByName("name") {
+				c.IndentedJSON(http.StatusOK, sys.Valves[i])
+
+			}
+		}
+	})
+	rest.GET("/pumps/:name", func(c *gin.Context) {
+		for i := range sys.Pumps {
+			if sys.Pumps[i].Name() == c.Params.ByName("name") {
+				c.IndentedJSON(http.StatusOK, sys.Pumps[i])
+
+			}
+		}
+	})
+	rest.GET("/status/:name", func(c *gin.Context) {
+		for i := range sys.Status {
+			if sys.Status[i].Name() == c.Params.ByName("name") {
+				c.IndentedJSON(http.StatusOK, sys.Status[i])
+
+			}
+		}
+	})
+	router.Run(":8080")
+}
---
+++ cmd/gateway/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/siro20/p1p2decoder/pkg/p1p2"
+	serial "go.bug.st/serial.v1"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	verbose  = kingpin.Flag("verbose", "Verbose mode.").Short('v').Bool()
+	ttyDev   = kingpin.Flag("chardev", "Path to tty device to read data from").Short('c').String()
+	dumpFile = kingpin.Flag("dump", "Path to regular file to read data from").Short('d').String()
+	baudrate = kingpin.Flag("baud", "Serial baud rate").Short('b').Int()
+	parity   = kingpin.Flag("parity", "Serial parity: none/even/odd").Default("none").Short('p').String()
+	stopbits = kingpin.Flag("stop", "Serial stop bits: 1/2").Short('s').Int()
+)
+
+func main() {
+	kingpin.Parse()
+
+	mode := serial.Mode{
+		BaudRate: 115200,
+		DataBits: 8,
+		Parity:   serial.NoParity,
+		StopBits: serial.OneStopBit,
+	}
+	if *baudrate > 0 {
+		mode.BaudRate = *baudrate
+	}
+	if *stopbits == 2 {
+		mode.StopBits = serial.TwoStopBits
+	}
+	if *parity == "even" {
+		mode.Parity = serial.EvenParity
+	} else if *parity == "odd" {
+		mode.Parity = serial.OddParity
+	}
+
+	if (ttyDev == nil || *ttyDev == "") && (dumpFile == nil || *dumpFile == "") {
+		log.Print("No input specified")
+		os.Exit(1)
+	}
+
+	go runHtml(p1p2.Sys)
+
+	for {
+		var scanner *bufio.Scanner
+		var closer io.Closer
+		// Poll on Serial to open (Testing)
+		if ttyDev != nil && *ttyDev != "" {
+			con, err := serial.Open(*ttyDev, &mode)
+			if err != nil {
+				if *verbose {
+					fmt.Printf("Failed to open TTY: %v\n", err)
+				}
+				time.Sleep(time.Second)
+				continue
+			}
+			scanner = bufio.NewScanner(con)
+			closer = con
+		} else if dumpFile != nil && *dumpFile != "" {
+
+			file, err := os.Open(*dumpFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			scanner = bufio.NewScanner(file)
+			closer = file
+		}
+
+		for scanner.Scan() {
+			var s string
+			if strings.Contains(scanner.Text(), ":") {
+				s = strings.Split(scanner.Text(), ":")[1]
+			}
+
+			// Remove whitespace
+			s = strings.ReplaceAll(s, ", 0x", "")
+			s = strings.ReplaceAll(s, " 0x", "")
+			s = strings.ReplaceAll(s, " ", "")
+
+			buf, err := hex.DecodeString(s)
+			if err != nil {
+				if *verbose {
+					fmt.Printf("Skipping invalid line in file: %s", scanner.Text())
+				}
+				continue
+			}
+
+			_, err = p1p2.Decode(buf)
+			if err != nil {
+				if *verbose {
+					fmt.Printf("Error decoding packet '%s': %v\n", s, err)
+				}
+				continue
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Print(err)
+		}
+		closer.Close()
+		if dumpFile != nil && *dumpFile != "" {
+			time.Sleep(time.Second * 99999) // Keep running
+		}
+	}
+
+}
