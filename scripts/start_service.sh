b3168a7f2fd07c8a120b22189b48cfc2c0e75cb3
Patrick Rudolph <rudolphpatrick05@gmail.com>
2022-12-18 14:09:02 +0100

    Add file for packaging
    Signed-off-by: Patrick Rudolph <rudolphpatrick05@gmail.com>

---
+++ scripts/start_service.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+systemctl daemon-reload
+
+if [ -f "/tmp/.p1p2gateway_restart" ]; then
+  systemctl restart $(cat /tmp/.p1p2gateway_restart)
+  systemctl enable $(cat /tmp/.p1p2gateway_restart)
+  rm /tmp/.p1p2gateway_restart
+fi
---
+++ scripts/stop_service.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+if [ -d "/tmp" ]; then
+  echo $(systemctl show p1p2gateway@*|grep Id|cut -d= -f2) > /tmp/.p1p2gateway_restart
+fi
+systemctl stop p1p2gateway@*
