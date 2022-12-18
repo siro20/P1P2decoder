47c9343403c448cf8c7da8a4e5e8503c4cf5d855
Patrick Rudolph <rudolphpatrick05@gmail.com>
2022-12-18 12:52:47 +0100

    pkg/p1p2: Add more unit tests for temperature
    Signed-off-by: Patrick Rudolph <rudolphpatrick05@gmail.com>

--- pkg/p1p2/temperature_test.go
+++ pkg/p1p2/temperature_test.go
@@ -18,3 +18,24 @@ func TestTemperatureRegisterCallback(t *testing.T) {
 		t.Error("Callback not invoked")
 	}
 }
+
+func TestTemperatureRegisterChangeCallback(t *testing.T) {
+	gotcha := false
+	TempLeavingWater.SetValue(0.0)
+
+	TemperatureRegisterChangeCallback(TempLeavingWater, func(newVal float32, oldVal float32) {
+		if newVal == 1.0 && oldVal == 0.0 {
+			gotcha = true
+		}
+	})
+	TempDomesticHotWater.SetValue(1.0)
+	if gotcha {
+		t.Error("Callback triggered, but shouldn't")
+	}
+
+	TempLeavingWater.SetValue(1.0)
+
+	if !gotcha {
+		t.Error("Callback not invoked")
+	}
+}
