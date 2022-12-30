901de85584966650a2cb1c823a1cef03d0161058
Patrick Rudolph <rudolphpatrick05@gmail.com>
2022-12-30 15:16:33 +0100

    cmd/gateway: Add more sensors to HTML GUI
    Signed-off-by: Patrick Rudolph <rudolphpatrick05@gmail.com>

--- cmd/gateway/html.go
+++ cmd/gateway/html.go
@@ -28,11 +28,16 @@ func runHtml(cfg HtmlConfig) {
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title":        "DAIKIN Heat pump",
-			"temperatures": p1p2.Sys.Temperatures,
-			"valves":       p1p2.Sys.Valves,
-			"pumps":        p1p2.Sys.Pumps,
-			"status":       p1p2.Sys.Status,
+			"title":         "DAIKIN Heat pump",
+			"temperatures":  p1p2.Sys.Temperatures,
+			"valves":        p1p2.Sys.Valves,
+			"pumps":         p1p2.Sys.Pumps,
+			"status":        p1p2.Sys.Status,
+			"software":      p1p2.Sys.Software,
+			"flow":          p1p2.Sys.Flow,
+			"energy":        p1p2.Sys.Energy,
+			"working_hours": p1p2.Sys.WorkingHours,
+			"time":          p1p2.Sys.Time,
 		})
 	})
 	rest := router.Group("/api/v1")
@@ -51,12 +56,26 @@ func runHtml(cfg HtmlConfig) {
 	rest.GET("/status", func(c *gin.Context) {
 		c.IndentedJSON(http.StatusOK, p1p2.Sys.Status)
 	})
+	rest.GET("/software", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, p1p2.Sys.Software)
+	})
+	rest.GET("/flow", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, p1p2.Sys.Flow)
+	})
+	rest.GET("/energy", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, p1p2.Sys.Energy)
+	})
+	rest.GET("/working_hours", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, p1p2.Sys.WorkingHours)
+	})
+	rest.GET("/time", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, p1p2.Sys.Time)
+	})
 
 	rest.GET("/temperatures/:name", func(c *gin.Context) {
 		for i := range p1p2.Sys.Temperatures {
 			if p1p2.Sys.Temperatures[i].Name() == c.Params.ByName("name") {
 				c.IndentedJSON(http.StatusOK, p1p2.Sys.Temperatures[i])
-
 			}
 		}
 	})
@@ -65,7 +84,6 @@ func runHtml(cfg HtmlConfig) {
 		for i := range p1p2.Sys.Valves {
 			if p1p2.Sys.Valves[i].Name() == c.Params.ByName("name") {
 				c.IndentedJSON(http.StatusOK, p1p2.Sys.Valves[i])
-
 			}
 		}
 	})
@@ -73,7 +91,6 @@ func runHtml(cfg HtmlConfig) {
 		for i := range p1p2.Sys.Pumps {
 			if p1p2.Sys.Pumps[i].Name() == c.Params.ByName("name") {
 				c.IndentedJSON(http.StatusOK, p1p2.Sys.Pumps[i])
-
 			}
 		}
 	})
@@ -81,7 +98,41 @@ func runHtml(cfg HtmlConfig) {
 		for i := range p1p2.Sys.Status {
 			if p1p2.Sys.Status[i].Name() == c.Params.ByName("name") {
 				c.IndentedJSON(http.StatusOK, p1p2.Sys.Status[i])
-
+			}
+		}
+	})
+	rest.GET("/software/:name", func(c *gin.Context) {
+		for i := range p1p2.Sys.Software {
+			if p1p2.Sys.Software[i].Name() == c.Params.ByName("name") {
+				c.IndentedJSON(http.StatusOK, p1p2.Sys.Software[i])
+			}
+		}
+	})
+	rest.GET("/flow/:name", func(c *gin.Context) {
+		for i := range p1p2.Sys.Flow {
+			if p1p2.Sys.Flow[i].Name() == c.Params.ByName("name") {
+				c.IndentedJSON(http.StatusOK, p1p2.Sys.Flow[i])
+			}
+		}
+	})
+	rest.GET("/energy/:name", func(c *gin.Context) {
+		for i := range p1p2.Sys.Energy {
+			if p1p2.Sys.Energy[i].Name() == c.Params.ByName("name") {
+				c.IndentedJSON(http.StatusOK, p1p2.Sys.Energy[i])
+			}
+		}
+	})
+	rest.GET("/working_hours/:name", func(c *gin.Context) {
+		for i := range p1p2.Sys.WorkingHours {
+			if p1p2.Sys.WorkingHours[i].Name() == c.Params.ByName("name") {
+				c.IndentedJSON(http.StatusOK, p1p2.Sys.WorkingHours[i])
+			}
+		}
+	})
+	rest.GET("/time/:name", func(c *gin.Context) {
+		for i := range p1p2.Sys.Time {
+			if p1p2.Sys.Time[i].Name() == c.Params.ByName("name") {
+				c.IndentedJSON(http.StatusOK, p1p2.Sys.Time[i])
 			}
 		}
 	})
